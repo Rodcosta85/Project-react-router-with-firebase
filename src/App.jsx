b2f8d1951349b3548067698191f93ed6103b51be
Fred Coutinho <fredcoutinho52@gmail.com>
2025-01-16 08:39:56 -0300

    adicionando título na página principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import AppRoutes from "./routes"
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <AppRoutes />
-    </div>
+    <>
+      <h1>Alterando o projeto</h1>
+      
+      <div className="flex flex-col justify-center items-center">
+        <AppRoutes />
+      </div>
+    </>
   );
 }
 
